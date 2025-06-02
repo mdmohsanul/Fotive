e5ba4cc4938354d8a51b470f161a7ff8aa5a236d
Mohsanul786 <mdmohsan2407@gmail.com>
2025-06-02 08:10:49 +0530

    Update auth.ts

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "https://fotive-backend.vercel.app//api/v1",
+  baseURL: "https://fotive-backend.vercel.app/api/v1",
   withCredentials: true, // Send and recieve cookies
 });
 
