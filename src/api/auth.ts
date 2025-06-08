a9fcf58063c98db7d8e629ac09c79fe30b84ae1b
Mohsanul786 <mdmohsan2407@gmail.com>
2025-06-08 07:21:00 +0530

    fix bugs

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const api = axios.create({
   // baseURL: "http://localhost:4000/api/v1",
-  baseURL: "https://fotive-backend.vercel.app/api/v1",
+  baseURL: "https://fotive.onrender.com/api/v1",
 
   withCredentials: true, // Send and recieve cookies
 });
